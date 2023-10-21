1f13ac01ecfa23e02080de0c37ed76b06f71ba92
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-10-21 20:49:16 +0700

    add destination category endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const destinationCategoryRouter = require("./destinationCategoryRouter");
+
+router.use("/api/destination-categories", destinationCategoryRouter);
+
 module.exports = router;
