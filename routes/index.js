8142ae86596b05df007864776545487608e7a8ef
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-10-21 21:54:11 +0700

    add article endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const destinationCategoryRouter = require("./destinationCategoryRouter");
 const destinationRouter = require("./destinationRouter");
+const articleRouter = require("./articleRouter");
 
 router.use("/api/destination-categories", destinationCategoryRouter);
 router.use("/api/destinations", destinationRouter);
+router.use("/api/articles", articleRouter);
 
 module.exports = router;
