353302ec4e92fb62682aede644306b7f57fe130d
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-10-21 20:20:32 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const routes = require("./routes/index");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
